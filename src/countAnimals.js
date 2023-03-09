5e3a9ba4d4e3772b7999579312ff76ad9552098a
Matheus Boechat <matheusfgb@gmail.com>
2023-03-09 15:20:28 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,15 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-const countAnimals = (animal) => {
-  // seu código aqui
+const countAnimals = (animals) => {
+  if (!animals) {
+    return species.reduce((acc, { name, residents }) => {
+      acc[name] = residents.length;
+      return acc;
+    }, {});
+  }
+  return species
+    .find((specie) => specie.name === animals.species).residents
+    .reduce((acc, resident) => ((!animals.sex || resident.sex === animals.sex) ? acc + 1 : acc), 0);
 };
 
 module.exports = countAnimals;
