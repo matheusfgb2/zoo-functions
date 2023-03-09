1d04833f5078188e72fd188f5c915c454fdd9513
Matheus Boechat <matheusfgb@gmail.com>
2023-03-09 15:42:39 -0300

    requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,11 @@
-const data = require('../data/zoo_data');
+const { species, employees } = require('../data/zoo_data');
 
-const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+const getOldestFromFirstSpecies = (employeeId) => {
+  const firstSpecieId = employees.find(({ id }) => id === employeeId).responsibleFor[0];
+  const { name, sex, age } = species
+    .find(({ id }) => id === firstSpecieId).residents
+    .sort((a, b) => b.age - a.age)[0];
+  return [name, sex, age];
 };
 
 module.exports = getOldestFromFirstSpecies;
