d807c91e6513edbef177b485325cfd99365a2a65
Matheus Boechat <matheusfgb@gmail.com>
2023-03-09 15:22:25 -0300

    requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,15 @@
-const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
 
-const countEntrants = (entrants) => {
-  // seu código aqui
-};
+const countEntrants = (entrants) => ({
+  child: entrants.filter(({ age }) => age < 18).length,
+  adult: entrants.filter(({ age }) => age < 50 && age >= 18).length,
+  senior: entrants.filter(({ age }) => age >= 50).length,
+});
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  if (!Array.isArray(entrants)) return 0;
+  const { child = 0, adult = 0, senior = 0 } = countEntrants(entrants);
+  return child * prices.child + adult * prices.adult + senior * prices.senior;
 };
 
 module.exports = { calculateEntry, countEntrants };
