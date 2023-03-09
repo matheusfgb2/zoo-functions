715ddaa74916aeeb7c5bc4914bb486b9b65250a4
Matheus Boechat <matheusfgb@gmail.com>
2023-03-09 20:32:05 -0300

    requisito 14

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,34 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+
+const getAllAnimalsMap = () => species
+  .reduce((acc, { location, name }) => {
+    acc[location] = acc[location] ? [...acc[location], name] : [name];
+    return acc;
+  }, {});
+
+const getAnimalsNames = (sorted, genre) => species
+  .reduce((acc, { location, name, residents }) => {
+    const specieResidents = genre
+      ? residents.filter((resident) => resident.sex === genre).map((resident) => resident.name)
+      : residents.map((resident) => resident.name);
+    const model = {
+      [name]: !sorted ? specieResidents : specieResidents.sort(),
+    };
+
+    acc[location] = acc[location] ? [...acc[location], model] : [model];
+    return acc;
+  }, {});
 
 const getAnimalMap = (options) => {
-  // seu código aqui
+  if (!options || !options.includeNames) {
+    return getAllAnimalsMap();
+  }
+  const { sex, sorted } = options;
+  if (sex) {
+    return getAnimalsNames(sorted, sex);
+  }
+  return getAnimalsNames(sorted);
 };
 
+console.log(getAnimalMap({ includeNames: true }));
 module.exports = getAnimalMap;
