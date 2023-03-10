98733f16c8ac910fb9c670adaafbfa0fe9dcee10
Matheus Boechat <matheusfgb@gmail.com>
2023-03-10 12:29:47 -0300

    organizando alguns detalhes da getAnimalMap

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,34 +1,33 @@
 const { species } = require('../data/zoo_data');
 
-const getAllAnimalsMap = () => species
+const getSpeciesMap = () => species
   .reduce((acc, { location, name }) => {
     acc[location] = acc[location] ? [...acc[location], name] : [name];
     return acc;
   }, {});
 
-const getAnimalsNames = (sorted, genre) => species
+const getNamesMap = (sorted, genre) => species
   .reduce((acc, { location, name, residents }) => {
     const specieResidents = genre
       ? residents.filter((resident) => resident.sex === genre).map((resident) => resident.name)
       : residents.map((resident) => resident.name);
-    const model = {
-      [name]: !sorted ? specieResidents : specieResidents.sort(),
+    const specieObject = {
+      [name]: sorted ? specieResidents.sort() : specieResidents,
     };
 
-    acc[location] = acc[location] ? [...acc[location], model] : [model];
+    acc[location] = acc[location] ? [...acc[location], specieObject] : [specieObject];
     return acc;
   }, {});
 
 const getAnimalMap = (options) => {
   if (!options || !options.includeNames) {
-    return getAllAnimalsMap();
+    return getSpeciesMap();
   }
   const { sex, sorted } = options;
   if (sex) {
-    return getAnimalsNames(sorted, sex);
+    return getNamesMap(sorted, sex);
   }
-  return getAnimalsNames(sorted);
+  return getNamesMap(sorted);
 };
 
-console.log(getAnimalMap({ includeNames: true }));
 module.exports = getAnimalMap;
